98c6c72c1d858b69a3ac3c3658e607c35c01079c
NgDD123 <ngiriyezadavid@gmail.com>
2025-12-15 10:20:40 +0200

    changes

--- client/src/components/Logo/index.jsx
+++ client/src/components/Logo/index.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-import botImage from "../../assets/botimage.jpg";
+import freedomLog from "../../assets/freedomlog.jpg";
 const Logo = () => {
   return (
     <div className="flex items-center gap-x-4">
-      <img className="w-[40px]" src={botImage} alt="Logo" />
+      <img className="w-[40px]" src={freedomLog} alt="Logo" />
       <Link
         to={"/"}
         className="hidden md:block text-white text-[26px] leading-[38.73px] font-bold"
